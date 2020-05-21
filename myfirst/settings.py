9cbf8ba13851edf4cc17a81132ed28e3b8c13d0b
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-05-21 22:59:56 +0300

    Update settings.py

--- myfirst/settings.py
+++ myfirst/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'myfirst.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lab4',
+        'NAME': 'default',
         'USER': 'egalievaa',
         'PASSWORD': 'ela',
         'HOST': 'egalievaa.mysql.pythonanywhere-services.com',
